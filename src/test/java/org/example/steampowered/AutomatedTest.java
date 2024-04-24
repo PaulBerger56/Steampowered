97286c465b51edbeff25afed5e5abb4131d5e87a
Albert <albert.austiniv@gmail.com>
2024-04-24 12:54:38 -0400

    Adjusted show more button css and added to auto test

--- src/test/java/org/example/steampowered/AutomatedTest.java
+++ src/test/java/org/example/steampowered/AutomatedTest.java
@@ -2,13 +2,17 @@ package org.example.steampowered;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class AutomatedTest {
 
     private static WebDriver driver;
+    private static final String username = "BasedTester";
+    private static final String password = "Ty?PX?:78*=vMN2";
 
     @BeforeAll
     public static void setUp(){
@@ -18,6 +22,16 @@ public class AutomatedTest {
 
     @Test
     public void testWheel() {
-        driver.get("http://127.0.0.1:8080/");
+//        driver.get("http://127.0.0.1:8080/");
+//        WebElement login = driver.findElement(By.xpath("/html/body/div/div/a"));
+//        login.click();
+//
+//        WebElement userNameField = driver.findElement(By.className("_2eKVn6g5Yysx9JmutQe7WV"));
+//        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[2]/input"));
+//
+//        userNameField.sendKeys(username);
+//        passwordField.sendKeys(password);
+//
+//        WebElement submit = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[4]/button"));
     }
 }
