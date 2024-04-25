2b3ce0f1028849d60babec55bd6d29a766dd1037
Albert <albert.austiniv@gmail.com>
2024-04-25 04:46:24 -0400

    added to auto test

--- src/test/java/org/example/steampowered/AutomatedTest.java
+++ src/test/java/org/example/steampowered/AutomatedTest.java
@@ -6,6 +6,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 
 public class AutomatedTest {
@@ -22,16 +25,30 @@ public class AutomatedTest {
 
     @Test
     public void testWheel() {
-//        driver.get("http://127.0.0.1:8080/");
-//        WebElement login = driver.findElement(By.xpath("/html/body/div/div/a"));
-//        login.click();
-//
-//        WebElement userNameField = driver.findElement(By.className("_2eKVn6g5Yysx9JmutQe7WV"));
-//        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[2]/input"));
-//
-//        userNameField.sendKeys(username);
-//        passwordField.sendKeys(password);
-//
-//        WebElement submit = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[4]/button"));
+        driver.get("http://127.0.0.1:8080/");
+        WebElement login = driver.findElement(By.xpath("/html/body/div/div/a"));
+        login.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        WebElement userNameField = driver.findElement(By.className("_2eKVn6g5Yysx9JmutQe7WV"));
+        userNameField.sendKeys(username);
+
+        WebElement passwordField = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[2]/input"));
+        passwordField.sendKeys(password);
+
+        WebElement signIn = driver.findElement(By.xpath("/html/body/div[1]/div[7]/div[4]/div[1]/div[1]/div/div/div/div[2]/div/form/div[4]/button"));
+        signIn.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        WebElement greenSignIn = driver.findElement(By.id("imageLogin"));
+        greenSignIn.click();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+
+        WebElement spinWheel = driver.findElement(By.id("spin"));
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
     }
 }
