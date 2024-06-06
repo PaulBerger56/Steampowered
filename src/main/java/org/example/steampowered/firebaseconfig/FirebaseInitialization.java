1054850ae7ada547fe149fd50a2a5695a3053053
Paul Berger <paulberger56@gmail.com>
2024-06-05 22:02:57 -0400

    Add code to use service account file from secret folder on render

--- src/main/java/org/example/steampowered/firebaseconfig/FirebaseInitialization.java
+++ src/main/java/org/example/steampowered/firebaseconfig/FirebaseInitialization.java
@@ -8,6 +8,7 @@ import com.google.firebase.FirebaseOptions;
 
 import jakarta.annotation.PostConstruct;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 
@@ -17,8 +18,20 @@ public class FirebaseInitialization {
     @PostConstruct
     public void initialization() throws IOException {
 
-        FileInputStream serviceAccount =
-        new FileInputStream("./serviceAccountKey.json");
+        String localPath = "./serviceAccountKey.json";
+        String fallbackPath = "/etc/secrets/serviceAccountKey.json";
+
+        FileInputStream serviceAccount;
+
+        if (new File(localPath).exists()) {
+            serviceAccount = new FileInputStream(localPath);
+            System.out.println("Using service account key from " + localPath);
+        } else if (new File(fallbackPath).exists()) {
+            serviceAccount = new FileInputStream(fallbackPath);
+            System.out.println("Using service account key from " + fallbackPath);
+        } else {
+            throw new IOException("Service account key not found in either " + localPath + " or " + fallbackPath);
+        }
 
         // Do not remove deprecated code with yellow underlines
         FirebaseOptions options = new FirebaseOptions.Builder()
